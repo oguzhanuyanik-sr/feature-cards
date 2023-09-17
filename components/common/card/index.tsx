1d3d5adf3264dd50a8e52d032e7d8024ecba248c
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2023-09-17 21:48:56 +0300

    framer motion added

--- components/common/card/index.tsx
+++ components/common/card/index.tsx
@@ -1,11 +1,16 @@
+'use client';
 import Image from 'next/image';
 import React from 'react';
 import { borderColors } from './constants';
 import { CardProps, ColorType } from './types';
+import { motion } from 'framer-motion';
 
 const Card = ({ index, title, text, img, color }: CardProps) => {
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.75 * (index + 1), type: 'spring' }}
       className={`border-t-4 p-[30px] shadow-card w-full lg:max-w-[350px] lg:h-1/2 lg:mt-0 ${
         index ? 'mt-[25px]' : ''
       } ${borderColors[color as ColorType]}`}
@@ -15,7 +20,7 @@ const Card = ({ index, title, text, img, color }: CardProps) => {
       <div className='w-full flex justify-end mt-10'>
         <Image src={img} alt='icon' height={64} width={64} sizes='100%' />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- components/containers/header-section/index.tsx
+++ components/containers/header-section/index.tsx
@@ -1,16 +1,23 @@
+'use client';
 import React from 'react';
 import { header } from '@/app/api/response.json';
+import { motion } from 'framer-motion';
 
 const HeaderSection = () => {
   return (
-    <header className='text-center'>
+    <motion.header
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1 }}
+      className='text-center'
+    >
       <h1 className='text-2xl lg:text-4xl font-extralight'>
         {header.title} <span className='font-semibold'>{header.boldTitle}</span>
       </h1>
       <p className='text-[15px] leading-[25px] mt-4 opacity-70'>
         {header.subTitle}
       </p>
-    </header>
+    </motion.header>
   );
 };
 
